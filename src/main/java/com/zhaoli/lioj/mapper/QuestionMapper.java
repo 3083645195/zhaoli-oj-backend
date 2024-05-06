14f850892a0489b1d15c325e0c00ef7d08a36ab6
立 <3083645195@qq.com>
2024-05-06 21:58:35 +0800

    feat: 表【question(题目)】的数据库操作Mapper

---
+++ src/main/java/com/zhaoli/lioj/mapper/QuestionMapper.java
@@ -0,0 +1,18 @@
+package com.zhaoli.lioj.mapper;
+
+import com.zhaoli.lioj.model.entity.Question;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 立
+* @description 针对表【question(题目)】的数据库操作Mapper
+* @createDate 2024-05-06 19:51:43
+* @Entity com.zhaoli.lioj.model.entity.Question
+*/
+public interface QuestionMapper extends BaseMapper<Question> {
+
+}
+
+
+
+
