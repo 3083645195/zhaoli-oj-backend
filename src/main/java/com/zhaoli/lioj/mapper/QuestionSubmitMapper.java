663f7b94230e2c1b5919bf02bac88f21d868c91e
立 <3083645195@qq.com>
2024-05-06 21:59:15 +0800

    feat: QuestionMapper.xml

---
+++ src/main/java/com/zhaoli/lioj/mapper/QuestionSubmitMapper.java
@@ -0,0 +1,18 @@
+package com.zhaoli.lioj.mapper;
+
+import com.zhaoli.lioj.model.entity.QuestionSubmit;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 立
+* @description 针对表【question_submit(题目提交)】的数据库操作Mapper
+* @createDate 2024-05-06 19:52:41
+* @Entity com.zhaoli.lioj.model.entity.QuestionSubmit
+*/
+public interface QuestionSubmitMapper extends BaseMapper<QuestionSubmit> {
+
+}
+
+
+
+
