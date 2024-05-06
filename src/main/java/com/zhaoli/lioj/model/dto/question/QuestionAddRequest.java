3c28844d07c9ec48864551c034f255cab9edb37e
立 <3083645195@qq.com>
2024-05-06 21:57:46 +0800

    新增DTO对象

---
+++ src/main/java/com/zhaoli/lioj/model/dto/question/QuestionAddRequest.java
@@ -0,0 +1,47 @@
+package com.zhaoli.lioj.model.dto.question;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 创建请求
+ *
+ * @author zhaoli
+ * @from
+ */
+@Data
+public class QuestionAddRequest implements Serializable {
+
+    /**
+     * 标题
+     */
+    private String title;
+
+    /**
+     * 内容
+     */
+    private String content;
+
+    /**
+     * 标签列表
+     */
+    private List<String> tags;
+
+    /**
+     * 题目答案
+     */
+    private String answers;
+
+    /**
+     * 判题用例
+     */
+    private List<JudgeCase> judgeCase;
+
+    /**
+     * 判题配置
+     */
+    private JudgeConfig judgeConfig;
+    private static final long serialVersionUID = 1L;
+}
