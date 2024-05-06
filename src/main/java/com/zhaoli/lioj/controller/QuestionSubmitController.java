d4a3fcf94de68130e45b97a60b70851d476c292e
立 <3083645195@qq.com>
2024-05-06 22:01:02 +0800

    feat:题目提交接口

---
+++ src/main/java/com/zhaoli/lioj/controller/QuestionSubmitController.java
@@ -0,0 +1,57 @@
+package com.zhaoli.lioj.controller;
+
+import com.zhaoli.lioj.common.BaseResponse;
+import com.zhaoli.lioj.common.ErrorCode;
+import com.zhaoli.lioj.common.ResultUtils;
+import com.zhaoli.lioj.exception.BusinessException;
+import com.zhaoli.lioj.model.dto.questionsubmit.QuestionSubmitAddRequest;
+import com.zhaoli.lioj.model.entity.User;
+import com.zhaoli.lioj.service.QuestionSubmitService;
+import com.zhaoli.lioj.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 题目提交接口
+ *
+ * @author zhaoli
+ * @from 
+ */
+@RestController
+@RequestMapping("/question_submit")
+@Slf4j
+public class QuestionSubmitController {
+
+    @Resource
+    private QuestionSubmitService questionSubmitService;
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 提交题目
+     *
+     * @param questionSubmitAddRequest
+     * @param request
+     * @return resultNum 本次点赞变化数
+     */
+    @PostMapping("/")
+    public BaseResponse<Integer> doQuestionSubmit(@RequestBody QuestionSubmitAddRequest questionSubmitAddRequest,
+            HttpServletRequest request) {
+        if (questionSubmitAddRequest == null || questionSubmitAddRequest.getQuestionId() <= 0) {
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        // 登录才能提交题目
+        final User loginUser = userService.getLoginUser(request);
+        long questionId = questionSubmitAddRequest.getQuestionId();
+        int result = questionSubmitService.doQuestionSubmit(questionId, loginUser);
+        return ResultUtils.success(result);
+    }
+
+}
